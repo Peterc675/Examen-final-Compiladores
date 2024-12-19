d48674d944810dd624fe31496594d470a71806f6
Peterc675 <143011197+Peterc675@users.noreply.github.com>
2024-12-19 15:39:00 -0400

    Add files via upload

---
+++ Examen Final Compiladores/Examen Final Compiladores/pregunta 1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Automata
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese una cadena (use los caracteres: m, h, a):");
+        string input = Console.ReadLine();
+
+        string estadoActual = "q0";
+
+        foreach (char c in input)
+        {
+            switch (estadoActual)
+            {
+                case "q0":
+                    if (c == 'm') estadoActual = "q1";
+                    else estadoActual = "q0"; 
+                    break;
+
+                case "q1":
+                    if (c == 'h') estadoActual = "q3";
+                    else if (c == 'm') estadoActual = "q2";
+                    else if (c == 'a') estadoActual = "q3";
+                    else estadoActual = "q0"; 
+                    break;
+
+                case "q2":
+                    if (c == 'h') estadoActual = "q2";
+                    else estadoActual = "q0"; 
+                    break;
+
+                case "q3":
+                    if (c == 'a') estadoActual = "q3";
+                    else estadoActual = "q0"; 
+                    break;
+
+                default:
+                    estadoActual = "q0";
+                    break;
+            }
+        }
+
+        if (estadoActual == "q3")
+        {
+            Console.WriteLine("Cadena aceptada.");
+        }
+        else
+        {
+            Console.WriteLine("Cadena no aceptada.");
+        }
+    }
+}
---
+++ Examen Final Compiladores/Pregunta2/Pregunta 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class GrammarValidator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la cadena para validar:");
+        string input = Console.ReadLine();
+
+        // Expresión regular basada en la gramática de la pregunta 3
+        string fileGrammar = @"^(\/(home|var|dir))+\/[a-zA-Z0-9_]+\.(txt|docx|ext)$";
+
+        if (Regex.IsMatch(input, fileGrammar))
+        {
+            Console.WriteLine("La cadena es válida según la gramática.");
+        }
+        else
+        {
+            Console.WriteLine("La cadena no es válida según la gramática.");
+        }
+    }
+}
